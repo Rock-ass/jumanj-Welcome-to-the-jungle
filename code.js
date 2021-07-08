bc5b19d0814b3e6e720c6c6b593138ffc45a8828
Rock-ass <72495466+Rock-ass@users.noreply.github.com>
2021-07-08 20:40:34 +0530

    Add files via upload

---
+++ code.js
@@ -0,0 +1,18 @@
+onEvent("button1", "click", function( ) {
+  setScreen("screen2");
+});
+onEvent("button3", "click", function( ) {
+  setScreen("screen3");
+});
+onEvent("button15", "click", function( ) {
+  open("https://rock-ass.github.io/Smolder-bravestone/index.html");
+});
+onEvent("button16", "click", function( ) {
+  open("https://rock-ass.github.io/ruby-roundhouse1/");
+});
+onEvent("button18", "click", function( ) {
+  open("https://rock-ass.github.io/mouse/");
+});
+onEvent("button17", "click", function( ) {
+  open("https://rock-ass.github.io/shelly-oberon/");
+});
